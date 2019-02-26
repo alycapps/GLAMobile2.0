eabd22070ab3e1edcc5abf3e2f1a83a229331087
Aly <38168385+alycapps@users.noreply.github.com>
2019-02-26 17:14:13 -0500

    fixed opacity of cards

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const Card = (props) => (
-  <div className="card">
+  <div className="card" style={{backgroundColor: 'rgba(255,255,255,0.95)'}}>
     <div className="card-header bg-dark" style={{color: '#fff'}}>
       <h5>{props.title}</h5>
     </div>
